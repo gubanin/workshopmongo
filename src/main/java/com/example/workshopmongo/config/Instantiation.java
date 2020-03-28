1ec8a96dd35241b32aa250e9d61ec49d9b350323
Gustavo <gustavo.banin@hotmail.com>
2020-03-28 19:59:28 -0300

    Referenciando os posts do usuario

--- src/main/java/com/example/workshopmongo/config/Instantiation.java
+++ src/main/java/com/example/workshopmongo/config/Instantiation.java
@@ -41,6 +41,9 @@ public class Instantiation implements CommandLineRunner {
 		Post p2 = new Post(null, sdf.parse("23/03/2018"), "Arrumei um trampo", "java junior!",new AuthorDTO(gu));
 
 		postRepository.saveAll(Arrays.asList(p1,p2));
+		
+		gu.getPosts().addAll(Arrays.asList(p1,p2));
+		userRepository.save(gu);
 	}
 
 }
